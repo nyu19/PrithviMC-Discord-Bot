53b5adfb2258170c3601e5fef70853e01608be20
Nakul Upasani <69073624+nyu19-py@users.noreply.github.com>
2022-09-29 22:48:57 +0530

    adds console spy changes made in gitignore to ignore __pycache__

--- tester.py
+++
@@ -1,7 +0,0 @@
-from mctools import PINGClient
-
-
-ping = PINGClient('vote.prithvimc.tk',25566)
-data = ping.get_stats()
-
-print(data)
--- utils/timeUtils.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime 
-
-def get_current_time():
-    return datetime.now().strftime("%H:%M:%S")
